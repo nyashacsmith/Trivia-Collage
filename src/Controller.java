7cc7bb16411372e8087cca7d3393b4ba03b2b5ad
nyashacsmith <nyashacsmith@gmail.com>
2016-11-11 18:30:14 -0500

    Controller class with blank actionListener

--- src/Controller.java
+++ src/Controller.java
@@ -6,45 +6,6 @@ import java.awt.event.ActionListener;
 
 
 public class Controller implements ActionListener {
-   public String category;
-    public class Category {
-        String category;
-        Question[] questions = new Question[25];
-        public Category(String c, Question[] q) {
-            category = c;
-            questions = q;
-        }
-        public void addQuestionArray(Question qu, int i) {questions[i] = qu;}
-        public Question[] getQuestions(){return questions;}
-    }
-
-
-        public class Question {
-            public String question;
-            public Answers[] answers = new Answers[4];
-        public Question(String q, Answers[] a) {
-             question = q;
-             answers = a;
-        }
-
-
-        public void addAnswerArray(Answers a, int index){answers[index] = a;}
-        public Answers[] getAnswers(){
-          return answers;
-        }
-
-    }
-
-        public class Answers extends JButton {
-            public String answer = " ";
-            public boolean right = false;
-            public Answers(String a, boolean b) {
-            answer = a;
-            boolean right = b;
-        }
-
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
 
